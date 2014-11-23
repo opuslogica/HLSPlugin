8339de9c42b3c22d1aad2c7d958e5da57397ae9b
Brian J. Fox <bfox@OpusLogica.com>
2014-11-23 11:22:57 -0800

    Attempt to include the Vitamio library with the plugin

---
+++ scripts/copy-libs.sh
@@ -0,0 +1,10 @@
+# copy-libs.sh: -*- Shell-script -*-  DESCRIPTIVE TEXT.
+# 
+#  Copyright (c) 2014 Brian J. Fox
+#  Author: Brian J. Fox (bfox@opuslogica.com) Sun Nov 23 11:15:42 2014.
+mydir=$(dirname $(dirname $0))
+if [[ $CORDOVA_CMDLINE == *android* ]]; then
+   echo "Copying Vitamio libraries into the project from the plugin.xml hook."
+   cp ${mydir}/Vitamio/Vitamio.jar platforms/android/libs
+   cp -r ${mydir}/Vitamio/libs/* platforms/android/libs
+fi
