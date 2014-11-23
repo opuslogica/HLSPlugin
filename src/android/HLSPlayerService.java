4bfedee6e909e4c9a2da13050c98bcf151547fe2
Brian J. Fox <bfox@OpusLogica.com>
2014-11-23 06:58:25 -0800

    Start getting serious about having a plugin

--- src/android/HLSPlayerService.java
+++ src/android/HLSPlayerService.java
@@ -18,165 +18,151 @@ import android.util.Log;
 import android.view.SurfaceView;
 
 public class HLSPlayerService extends Service {
-	
-	private static MediaPlayer mMediaPlayer;
 
-	private final static String TAG = HLSPlayerService.class.getSimpleName();
+    private static MediaPlayer mMediaPlayer;
 
-	// This is the object that receives interactions from clients. See
-	// RemoteService for a more complete example.
-	private final IBinder mBinder = new LocalBinder();
-	
-	/**
-	 * TODO: Set the path variable to a local audio file path.
-	 */
-	private static final String PATH = "http://cdns840stu0010.multistream.net/rtvcRadionicalive/smil:rtvcRadionica.smil/playlist.m3u8";
-	
-	
-	private String path = PATH;
-	
-	@Override
-	public void onCreate() {
-		Log.i(TAG, "CREATE");
-		
+    private final static String TAG = HLSPlayerService.class.getSimpleName();
+
+    // This is the object that receives interactions from clients. See
+    // RemoteService for a more complete example.
+    private final IBinder mBinder = new LocalBinder();
+
+    /**
+     * TODO: Set the path variable to a local audio file path.
+     */
+    private static final String PATH = "http://cdns840stu0010.multistream.net/rtvcRadionicalive/smil:rtvcRadionica.smil/playlist.m3u8";
+    private String path = PATH;
+
+    @Override
+    public void onCreate() {
+	Log.i(TAG, "CREATE");
+    }
+
+    public String getPath() {
+	return path;
+    }
+
+    public void setPath(String path) {
+	this.path = path;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+	Log.i(TAG, "Received start id " + startId + ": " + intent);
+
+	if (mMediaPlayer != null){
+	    Log.i(TAG, "HLSPlayer: onStartCommand: already have a MediaPlayer");
+	    return START_STICKY;
 	}
-	
 
-	public String getPath() {
-		return path;
-	}
+	if (isNetworkOnline()) {
+	    Log.i(TAG, "HLSPlayer: onStartCommand: Connecting to the Internet");
+	    try {
+		Log.i(TAG, "HLSPlayer: onStartCommand: start playing for " + path);
 
-	public void setPath(String path) {
-		this.path = path;
-	}
+		mMediaPlayer = new MediaPlayer(getApplicationContext());
+		mMediaPlayer.setDataSource(path);
+		mMediaPlayer.setAdaptiveStream(true);
+		mMediaPlayer.setBufferSize(1024 * 512);
+		mMediaPlayer.prepare();
 
+		mMediaPlayer.setOnErrorListener(onErrorListener);
+		mMediaPlayer.setOnBufferingUpdateListener(new OnBufferingUpdateListener() {
+			@Override
+			public void onBufferingUpdate(MediaPlayer mp, int percent) {
+			    // TODO Auto-generated method stub
 
-
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		Log.i(TAG, "Received start id " + startId + ": " + intent);
-
-		if(mMediaPlayer != null){
-			Log.i(TAG, "Retornando el que ya estaba");
-			return START_STICKY;
-		}
-
-		if (isNetworkOnline()) {
-			Log.i(TAG, "CONECTADO A INTERNET");
-			try {
-				
-				Log.i(TAG, "Iniciando Streaming desde " + path);
-				
-				mMediaPlayer = new MediaPlayer(getApplicationContext());
-				//mMediaPlayer.setDisplay(new SurfaceView(this).getHolder());
-				mMediaPlayer.setDataSource(path);
-				mMediaPlayer.setAdaptiveStream(true);
-				mMediaPlayer.setBufferSize(1024 * 512);
-				mMediaPlayer.prepare();
-				
-				
-				mMediaPlayer.setOnErrorListener(onErrorListener);
-				mMediaPlayer.setOnBufferingUpdateListener(new OnBufferingUpdateListener() {
-					
-					@Override
-					public void onBufferingUpdate(MediaPlayer mp, int percent) {
-						// TODO Auto-generated method stub
-						
-						Log.i(TAG, "Buffer actualizado  al " + percent +"%");
-						if(percent >= 20 && !isPlaying()){
-							mMediaPlayer.start();
-						}
-				
-					}
-				});
-				
-
-			} catch (IllegalArgumentException e) {
-				e.printStackTrace();
-			} catch (SecurityException e) {
-				e.printStackTrace();
-			} catch (IllegalStateException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
+			    Log.i(TAG, "HLSPlayer: onStartCommand: amount buffered so far: " + percent + "%");
+			    if (percent >= 20 && !isPlaying()) {
+				Log.i(TAG, "HLSPlayer: onStartCommand: starting to play because more than 20% buffered.");
+				mMediaPlayer.start();
+			    }
 			}
-		} else {
-			Log.e(TAG, "We are out of connection... playin local audio");
-		}
-
-		// We want this service to continue running until it is explicitly
-		// stopped, so return sticky.
-		return START_STICKY;
+		    });
+	    } catch (IllegalArgumentException e) {
+		e.printStackTrace();
+	    } catch (SecurityException e) {
+		e.printStackTrace();
+	    } catch (IllegalStateException e) {
+		e.printStackTrace();
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
+	} else {
+	    Log.e(TAG, "HLSPlayer: onStartCommand: the network is offline.  Play something to that effect?");
 	}
 
-	private OnErrorListener onErrorListener = new OnErrorListener() {
-		@Override
-		public boolean onError(MediaPlayer mp, int what, int extra) {
-			Log.e(TAG, "Error en el player ");
-			return true;
-		}
+	// We want this service to continue running until it is explicitly
+	// stopped, so return sticky.
+	return START_STICKY;
+    }
+
+    private OnErrorListener onErrorListener = new OnErrorListener() {
+	    @Override
+	    public boolean onError(MediaPlayer mp, int what, int extra) {
+		Log.e(TAG, "HLSPlayer: OnErrorListener: Got some error in the player.");
+		return true;
+	    }
 	};
 
-	private boolean isNetworkOnline() {
-		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo netInfo = cm.getActiveNetworkInfo();
-		if (netInfo != null && netInfo.isConnectedOrConnecting()) {
-			return true;
-		}
-		return false;
+    private boolean isNetworkOnline() {
+	ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+	NetworkInfo netInfo = cm.getActiveNetworkInfo();
+	if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+	    return true;
 	}
+	return false;
+    }
 
-	private void stopMediaPlayer(){
-		if (mMediaPlayer != null) {
-			if (mMediaPlayer.isPlaying()) {
-				mMediaPlayer.stop();
-				mMediaPlayer.release();
-				mMediaPlayer = null;
-			} else {
-				mMediaPlayer.stop();
-				mMediaPlayer.release();
-				mMediaPlayer = null;
-			}
-		}
+    private void stopMediaPlayer(){
+	if (mMediaPlayer != null) {
+	    if (mMediaPlayer.isPlaying()) {
+		mMediaPlayer.stop();
+		mMediaPlayer.release();
+		mMediaPlayer = null;
+	    } else {
+		mMediaPlayer.stop();
+		mMediaPlayer.release();
+		mMediaPlayer = null;
+	    }
 	}
+    }
 
-	public void stopStreaming() {
-		stopMediaPlayer();
-	}
+    public void stopStreaming() {
+	stopMediaPlayer();
+    }
 
-	@Override
-	public void onDestroy() {
-		Log.i(TAG, "Destroy en el Servicio");
-		stopMediaPlayer();
-		super.onDestroy();
-	}
-	
-	public boolean isPlaying(){
-		if(mMediaPlayer != null && mMediaPlayer.isPlaying()){
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public void setVolume(float volume){
-		mMediaPlayer.setVolume(volume, volume);
-	}
-	
+    @Override
+    public void onDestroy() {
+	Log.i(TAG, "HLSPlayer: onDestroy: Destroying the player.");
+	stopMediaPlayer();
+	super.onDestroy();
+    }
 
-	@Override
-	public IBinder onBind(Intent arg0) {
-		return mBinder;
+    public boolean isPlaying(){
+	if (mMediaPlayer != null && mMediaPlayer.isPlaying()) {
+	    return true;
+	} else {
+	    return false;
 	}
+    }
 
-	/**
-	 * Class for clients to access. Because we know this service always runs in
-	 * the same process as its clients, we don't need to deal with IPC.
-	 */
-	public class LocalBinder extends Binder {
-		public HLSPlayerService getService() {
-			return HLSPlayerService.this;
-		}
+    public void setVolume(float volume){
+	mMediaPlayer.setVolume(volume, volume);
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+	return mBinder;
+    }
+
+    /**
+     * Class for clients to access. Because we know this service always runs in
+     * the same process as its clients, we don't need to deal with IPC.
+     */
+    public class LocalBinder extends Binder {
+	public HLSPlayerService getService() {
+	    return HLSPlayerService.this;
 	}
-
+    }
 }
--- src/android/HLSPlugin.java
+++ src/android/HLSPlugin.java
@@ -14,130 +14,111 @@ import android.os.IBinder;
 import android.util.Log;
 
 public class HLSPlugin extends CordovaPlugin {
-	
-	private static final String TAG = HLSPlugin.class.getSimpleName();
-	
-	private HLSPlayerService service;
-	
-	private String resourceURL;
-	private String id;
-	
-	@Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		
-		Log.d(TAG, "Ha llegado el mensaje de invocacion, action " + action + " args "+ args.toString());
 
-		PluginResult.Status status = PluginResult.Status.OK;
-		
-		if("create".equalsIgnoreCase(action)){
-			
-			id = args.getString(0);
+    private static final String TAG = HLSPlugin.class.getSimpleName();
+
+    private HLSPlayerService service;
+
+    private String resourceURL;
+    private String id;
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+	Log.d(TAG, "HLSPlugin: execute(" + action + ", "+ args.toString(), ")");
+	PluginResult.Status status = PluginResult.Status.OK;
+
+	if ("create".equalsIgnoreCase(action)) {
+	    id = args.getString(0);
             resourceURL = args.getString(1);
-            
-            Intent intent= new Intent(this.cordova.getActivity(), HLSPlayerService.class);
+
+            Intent intent = new Intent(this.cordova.getActivity(), HLSPlayerService.class);
             this.cordova.getActivity().bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-    		
-			
-		} else if ("startPlayingAudio".equalsIgnoreCase(action)){
-			
-			this.cordova.getActivity().startService(new Intent(this.cordova.getActivity(), HLSPlayerService.class));
-		
-		} else if ("stopPlayingAudio".equalsIgnoreCase(action)){
-			
-			stopService();
-			
-		} else if ("seekToAudio".equalsIgnoreCase(action)){
-			
-		} else if ("pausePlayingAudio".equalsIgnoreCase(action)){
-			
-		} else if ("getCurrentPositionAudio".equalsIgnoreCase(action)){
-			float f = 0;
+
+	} else if ("startPlayingAudio".equalsIgnoreCase(action)) {
+	    this.cordova.getActivity().startService(new Intent(this.cordova.getActivity(), HLSPlayerService.class));
+
+	} else if ("stopPlayingAudio".equalsIgnoreCase(action)){
+	    stopService();
+
+	} else if ("seekToAudio".equalsIgnoreCase(action)) {
+	} else if ("pausePlayingAudio".equalsIgnoreCase(action)) {
+	    /* Pause the fucking player for chrissakes. */
+	} else if ("getCurrentPositionAudio".equalsIgnoreCase(action)) {
+	    float f = 0;
             callbackContext.sendPluginResult(new PluginResult(status, f));
             return true;
-		} else if ("setVolume".equalsIgnoreCase(action)){
-			try{
-				service.setVolume(100 * Float.parseFloat(args.getString(1)));
-				Log.i(TAG, "setVolume "+ Float.parseFloat(args.getString(1)));
-			} catch (Exception ex){
-				
-			}
-			return true;
-		} else {
-			return true;
-		}
-		
-		callbackContext.sendPluginResult(new PluginResult(status, ""));
+	} else if ("setVolume".equalsIgnoreCase(action)) {
+	    try {
+		service.setVolume(100 * Float.parseFloat(args.getString(1)));
+		Log.i(TAG, "HLSPlugin: execute: setVolume(" + Float.parseFloat(args.getString(1)) + ")");
+	    } catch (Exception ex) {
+		Log.e(TAG, "HLSPlugin: execute: setVolume: ERROR");
+	    }
+	    return true;
+	} else {
+	    return true;
+	}
+
+	callbackContext.sendPluginResult(new PluginResult(status, ""));
 
         return true;
-	}
-	
-	
-	private void stopService(){
-		if(service != null){
-			service.stopStreaming();
-			service.stopService(new Intent(this.cordova.getActivity(), HLSPlayerService.class));
-		} else {
-			this.cordova.getActivity().stopService(new Intent(this.cordova.getActivity(), HLSPlayerService.class));
-		}
-		Log.i(TAG, "Detener");
-	}
-	
-	
-	@Override
-	public void onResume(boolean multitasking) {
-		// TODO Auto-generated method stub
-		super.onResume(multitasking);
-				
-	}
-	
-	@Override
-	public void onPause(boolean multitasking) {
-		// TODO Auto-generated method stub
-		super.onPause(multitasking);
-		//this.cordova.getActivity().unbindService(mConnection);
-		//Log.i(TAG, "onPause - Desconectarse desde el Plugin");
-	}
+    }
 
-
-	@Override
-	public void onReset() {
-		// TODO Auto-generated method stub
-		super.onReset();
+    private void stopService() {
+	if (service != null) {
+	    service.stopStreaming();
+	    service.stopService(new Intent(this.cordova.getActivity(), HLSPlayerService.class));
+	} else {
+	    this.cordova.getActivity().stopService(new Intent(this.cordova.getActivity(), HLSPlayerService.class));
 	}
+	Log.i(TAG, "HLSPlugin: stopService: STOPPED");
+    }
 
+    @Override
+    public void onResume(boolean multitasking) {
+	// TODO Auto-generated method stub
+	super.onResume(multitasking);
+    }
 
+    @Override
+    public void onPause(boolean multitasking) {
+	// TODO Auto-generated method stub
+	super.onPause(multitasking);
+    }
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		//stopService();
-		this.cordova.getActivity().unbindService(mConnection);
-		Log.i(TAG, "onDestroy - Desconectarse desde el Plugin");
-	}
-	
-	private ServiceConnection mConnection = new ServiceConnection() {
+    @Override
+    public void onReset() {
+	// TODO Auto-generated method stub
+	super.onReset();
+    }
 
+    @Override
+    public void onDestroy() {
+	super.onDestroy();
+	this.cordova.getActivity().unbindService(mConnection);
+	Log.i(TAG, "HLSPlugin: onDestroy - disconnected the plugin");
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
 	    public void onServiceConnected(ComponentName className, IBinder binder) {
 	    	HLSPlayerService.LocalBinder b = (HLSPlayerService.LocalBinder) binder;
 	    	service = b.getService();
-	    	Log.i(TAG, "Servicio Connectado");
-	    	
+	    	Log.i(TAG, "HLSPlugin: onServiceConnected: Connected to service");
+
 	    	boolean playing = service.isPlaying();
-	    	if(resourceURL != null){
-	    		service.setPath(resourceURL);
+	    	if (resourceURL != null) {
+		    service.setPath(resourceURL);
 	    	}
-	    	
-			if(playing){
-				//showStop();
-			}  else {
-				//showPlay();
-			}
+
+		if(playing){
+		    //showStop();
+		}  else {
+		    //showPlay();
+		}
 	    }
 
 	    public void onServiceDisconnected(ComponentName className) {
-	      service = null;
-	      Log.i(TAG, "Servicio Desconnectado");
-	    }
-	  };
-
+		service = null;
+	    	Log.i(TAG, "HLSPlugin: onServiceDisconnected: Disconnected from service");
+	};
 }
